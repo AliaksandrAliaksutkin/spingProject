6211c7945d1c1d57ebbff3460592f1274fae9b02
Alexander <61922116+Lazovski1991@users.noreply.github.com>
2021-10-18 21:55:44 +0000

    Update User.java

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -11,7 +11,7 @@ public class User {
     @Id                                                     // Определяет простой первичный ключ, состоящий из одного поля;
     @GeneratedValue(strategy = GenerationType.SEQUENCE)     //автоматическая генерация значения первичного ключа
     @Column(name = "id_user")
-    private int id;
+    private int id; //здесь лучше Long
     @Column(name = "first_name")
     private String firstName;
     @Column(name = "last_name")
